5bb81a728b938a0a883212f7edbf9672a29782b7
rmmcosta-hibob <ricardo.costa@hibob.io>
2024-04-06 18:10:31 +0100

    feat: using scala 3 enums

---
+++ searchArtistsWithEnumsScala3.scala
@@ -0,0 +1,85 @@
+import model._
+import Genre._
+
+object Main extends App {
+  println(
+    s"should print 2 alternative rock artists ${searchArtists(goodArtists, List(AlternativeRock))}"
+  )
+  println(
+    s"should print 3 USA artists ${searchArtists(goodArtists, locations = List(Location("USA")))}"
+  )
+  println(
+    s"should print 1 artist between 1980 and 1993 ${searchArtists(goodArtists, searchByActiveYears = true, activeSince = 1980, activeUntil = 1993)}"
+  )
+  println(
+    s"should print 1 artist between 2007 and 2023 ${searchArtists(goodArtists, searchByActiveYears = true, activeSince = 2007, activeUntil = 2023)}"
+  )
+}
+
+def goodArtists = List(
+  Artist(
+    "Linkin Park",
+    AlternativeRock,
+    Location("USA"),
+    2002,
+    false,
+    2019
+  ),
+  Artist("Led Zepplin", HardRock, Location("England"), 1980, false, 1990),
+  Artist("Smashing Pumpkins", Rock, Location("USA"), 1980, false, 1993),
+  Artist(
+    "30 Seconds to Mars",
+    AlternativeRock,
+    Location("USA"),
+    2007,
+    true,
+    0
+  )
+)
+
+def searchArtists(
+    artists: List[Artist],
+    genres: List[Genre] = List.empty,
+    locations: List[Location] = List.empty,
+    searchByActiveYears: Boolean = false,
+    activeUntil: Int = 0,
+    activeSince: Int = 0
+): List[Artist] = {
+  val filteredByGenre: List[Artist] =
+    artists.filter(artist => genres.isEmpty || genres.contains(artist.genre))
+  val filteredByLocation: List[Artist] = filteredByGenre.filter(artist =>
+    locations.isEmpty || locations.contains(artist.origin)
+  )
+  if (searchByActiveYears)
+    filteredByLocation.filter(artist =>
+      artist.yearsActiveStart <= activeSince && (artist.isActive || artist.yearsActiveEnd >= activeUntil)
+    )
+  else
+    filteredByLocation
+}
+
+case class Artist(
+    name: String,
+    genre: Genre,
+    origin: Location,
+    yearsActiveStart: Int,
+    isActive: Boolean,
+    yearsActiveEnd: Int
+)
+
+//we need to declare the opaque type inside another object because if the opaque type is in the same context as it's use it will allow strings and not
+//obly us to use Location("something")
+object model {
+  opaque type Location = String
+
+  object Location {
+    def apply(value: String): Location = value
+    extension (value: Location) def name: String = value
+  }
+}
+
+enum Genre(val name: String) {
+  case AlternativeRock extends Genre("Alternative Rock")
+  case HardRock extends Genre("Hard Rock")
+  case Rock extends Genre("Rock")
+}
